ee6f35e32112efbf3dd8161ef9cd603786298aca
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-12-24 23:47:46 -0300

    Add project files.

---
+++ Petfolio.API/Controllers/PetController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Petfolio.Communication.Requests;
+
+namespace Petfolio.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class PetController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public IActionResult Register([FromBody] RequestRegisterPetDto dto)
+    {
+        return Created();
+    }
+}
